5480c29864ba58b4e4514b9d4e20c996abcf9cee
hubo <huboc@yonyou.com>
2016-03-08 16:23:31 +0800

    添加code验证

---
+++ openapi-server/src/main/java/com/yonyou/mcloud/interceptors/RequestCountInterceptor.java
@@ -0,0 +1,31 @@
+package com.yonyou.mcloud.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 访问次数限制
+ * Created by hubo on 2016/3/8
+ */
+public class RequestCountInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+
+}
